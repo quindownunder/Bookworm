17b81b2d565f80b1db57ac1a63d9e7838141a785
Matthew Richardson <mat_richardson@mac.com>
2020-09-01 14:58:39 +1000

    Basic app complete

--- Bookworm/AddBookView.swift
+++ Bookworm/AddBookView.swift
@@ -11,6 +11,8 @@ import SwiftUI
 struct AddBookView: View {
     
     @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    
     
     @State private var title = ""
     @State private var author = ""
@@ -35,18 +37,22 @@ struct AddBookView: View {
                 }
                 
                 Section {
-                    Picker("Rating", selection: $rating) {
-                        ForEach(0..<6) {
-                            Text("\($0)")
-                        }
-                    }
-                    
-                    TextField("Wriate a review", text: $review)
+                    RatingView(rating: $rating)
+                    TextField("Write a review", text: $review)
                 }
                 
                 Section {
                     Button("Save") {
+                        let newBook = Book(context: self.moc)
+                        newBook.title = self.title
+                        newBook.author = self.author
+                        newBook.rating = Int16(self.rating)
+                        newBook.genre = self.genre
+                        newBook.review = self.review
                         
+                        try? self.moc.save()
+                        
+                        self.presentationMode.wrappedValue.dismiss()
                     }
                 }
             }
